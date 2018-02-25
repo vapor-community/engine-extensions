26a9522a4953d8d27e3c55e6b7821fd1faece552
Alex Reilly <alexander.r.reilly@gmail.com>
2018-02-24 22:53:25 -0800

    Adds documetation

--- Sources/EngineExtensions/Future+WebSocket.swift
+++ Sources/EngineExtensions/Future+WebSocket.swift
@@ -2,6 +2,18 @@ import Async
 import WebSocket
 import Foundation
 
+
+/// EngineExtensions: Sends the expectation of self to a websocket.
+///
+///    router.websocket("demo") { (req, ws) in
+///         ws.onData({ (ws, msg) in
+///            _ = Future(msg)
+///                 .send(to: ws)
+///         })
+///     }
+///
+/// - Parameter websocket: The `WebSocket` to send the expectation to.
+/// - Returns: Passes through the expectation of `self` after sending to `websocket`
 extension Future where Expectation == String {
     public func send(to websocket: WebSocket) -> Future<Expectation> {
         return self.flatMap(to: Expectation.self) { (data) in
@@ -11,6 +23,18 @@ extension Future where Expectation == String {
     }
 }
 
+
+/// EngineExtensions: Sends the expectation of self to a websocket.
+///
+///    router.websocket("demo") { (req, ws) in
+///         ws.onString({ (ws, msg) in
+///            _ = Future(msg)
+///                 .send(to: ws)
+///         })
+///     }
+///
+/// - Parameter websocket: The `WebSocket` to send the expectation to,
+/// - Returns: Passes through the expectation of `self` after sending to `websocket`
 extension Future where Expectation == Data {
     public func send(to websocket: WebSocket) -> Future<Expectation> {
         return self.flatMap(to: Expectation.self) { (data) in
